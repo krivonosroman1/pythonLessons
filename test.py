22dc58fa2ea87d0fcaca7b6c2755934afa7d3f29
Ivan <ivan.k@filancy.com>
2019-08-27 18:44:55 +0300

    fixed bugs

--- test.py
+++ test.py
@@ -2,6 +2,6 @@ my_name = "Roma"
 my_age = 10
 your_name = input("Как тебя зовут? ")
 your_age = input("Сколько тебе лет? ")
-print("Меня зовут", my_name ", и мне", my_age, "лет.")
+print("Меня зовут", my_name, ",и мне", my_age, "лет.")
 print("тебя зовут", your_name, ",и тебе", your_age, ".")
 print("Спасибо за покупку моей книги,", your_name, "!")
