223e6bc26e142568cca31ca717433e527103ccf0
Ivan <ivan.k@filancy.com>
2019-08-27 18:35:38 +0300

    24

---
+++ 24.py
@@ -0,0 +1,18 @@
+Python 3.6.7 (default, Oct 22 2018, 11:32:17) 
+[GCC 8.2.0] on linux
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> 4+2
+6
+>>> 4-2
+2
+>>> 4*2
+8
+>>> 4\2
+SyntaxError: unexpected character after line continuation character
+>>> 4**2
+16
+>>> (4+2)*3
+18
+>>> 4/2
+2.0
+>>> 
---
+++ git2.py
@@ -0,0 +1,25 @@
+Python 3.6.7 (default, Oct 22 2018, 11:32:17) 
+[GCC 8.2.0] on linux
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> 3 + 3
+6
+>>> x=5
+>>> 5
+5
+>>> x*2
+10
+>>> x*2
+10
+>>> x/2
+2.5
+>>> x+2
+7
+>>> x-2
+3
+>>> x=9
+>>> x
+9
+>>> x=x-7
+>>> x
+2
+>>> 
